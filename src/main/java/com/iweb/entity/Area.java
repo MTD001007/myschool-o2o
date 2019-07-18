9d521cd948a0f09264104e8dfd67a26ef6c471a4
MTD001007 <xiaoboshiwh@126.com>
2019-07-18 11:38:38 +0800

    Initial commit

---
+++ src/main/java/com/iweb/entity/Area.java
@@ -0,0 +1,15 @@
+package com.iweb.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Area {
+  private Long areaId;
+  private String areaName;
+  private String areaDesc;
+  private Integer priority;
+  private Date createTime;
+  private Date lastEditTime;
+}
---
+++ src/main/java/com/iweb/entity/Shop.java
@@ -0,0 +1,35 @@
+package com.iweb.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class Shop {
+
+  private Long shopId;
+  private Long ownerId;
+  private Long shopCategoryId;
+  private String shopName;
+  private String shopDesc;
+  private String shopAddr;
+  private String phone;
+  private String shopImg;
+  private Double longitude;
+  private Double latitude;
+  private Integer priority;
+  private Date createTime;
+  private Date lastEditTime;
+  //-1.不可用  0.审核中  1.可用
+  private Integer enableStatus;
+  //超级管理员给店家的提醒
+  private String advice;
+
+  private List<ShopAuthMap> staffList;
+  private Area area;
+  private ShopCategory shopCategory;
+  private ShopCategory parentCategory;
+}
+ 
+ 
